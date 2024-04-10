d120218f8b23eadbc719e994c08f88c92a2b75c9
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-10 19:40:30 +0400

    employee and employeeList added to template

---
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -0,0 +1,62 @@
+package com.cydeo.bootstrap;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator {
+
+    public static List<String> getAllStates(){
+        return Arrays.asList(
+                "Alaska",
+                "Alabama",
+                "Arkansas",
+                "Arizona",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "Florida",
+                "Georgia",
+                "Hawaii",
+                "Iowa",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Massachusetts",
+                "Maryland",
+                "Maine",
+                "Michigan",
+                "Minnesota",
+                "Missouri",
+                "Mississippi",
+                "Montana",
+                "North Carolina",
+                "North Dakota",
+                "Nebraska",
+                "New Hampshire",
+                "New Jersey",
+                "New Mexico",
+                "Nevada",
+                "New York",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Pennsylvania",
+                "Rhode Island",
+                "South Carolina",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Virginia",
+                "Vermont",
+                "Washington",
+                "Wisconsin",
+                "West Virginia",
+                "Wyoming"
+        );
+    }
+
+}

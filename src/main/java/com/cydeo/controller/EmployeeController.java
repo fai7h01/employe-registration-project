bc5963b3802016f7789edfbd648645f5827d0cc2
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-10 19:47:39 +0400

    capture employee object

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +20,14 @@ public class EmployeeController {
         model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "/employee/employee-create";
+    }
+
+    @PostMapping("/insert")
+    public String employee(@ModelAttribute("employee") Employee employee){
+
+        System.out.println(employee);
+
+        return "redirect:/employee/register";
     }
 
 }

9d6f77c08595bc5108821d1ff31c929184e1ff44
fai7h01 <l.varsimashvil10@gmail.com>
2024-04-11 16:42:10 +0400

    validation for first and last name

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,9 +1,6 @@
 package com.cydeo.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -27,15 +24,25 @@ public class Employee {
     @NotBlank
     @Size(max = 12, min = 2)
     private String firstName;
+
+    @NotBlank
+    @Size(max = 12, min = 2)
     private String lastName;
 
     // Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy  -  yyyy-dd-mm
 
+//    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
+//    @NotBlank
+//    @Email
     private String email;
+
+//    @NotBlank
+//    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String password;
+
     private String address;
     private String address2;
     private String city;
